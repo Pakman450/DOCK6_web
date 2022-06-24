a9b4778637efb9f2ae61ba9ef89963b7df514607
Pakman450 <steven.pak10@gmail.com>
2022-06-24 12:39:07 -0400

    fix: move res.end() to the correct place

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const http = require('http')
-const port = 3000
 const fs = require('fs')
+const port = 3000
+
 
 const server = http.createServer(function(req,res){
-    // res.writeHead(200, { 'Content-Type': 'text/html' })
-    // fs.readFile('index.html',function(error,data){
-    //     if (error){
-    //         res.writeHead(404)
-    //         res.write('Error: File Not Found')
-    //     } else {
-    //         res.write(data)
-    //     }
-    // })
-    res.write('lol')
-    res.end()
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html',function(error,data){
+        if (error){
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+    // res.write('lol')
+    // res.end()
 
 })
 
