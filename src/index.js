de0bed37fce25697eb043456eaad520e26033f74
Pakman450 <steven.pak10@gmail.com>
2022-06-24 13:44:44 -0400

    feat(frontend): add index.js to give functionality to front end

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+console.log('start')
+const lets_dock = document.getElementById('lets_dock')
+
+
+//this is where it gets complicated
+//if hover, shows the relevant steps to get a docking goign
+//planning to show Prepare Files then Let's Dock!
+function toLetsDock(){
+
+}
+
+//this is where if you hover above the about button it will
+//show..[INCOMPLETE]
+function toAbout(){
+
+}
+
+//this is where if you click on it, you will go to the member's page
+function toMembers(){
+
+}
+
+//if click, a page to show how to download dock with a link to the github repo
+function toDownloadDock(){
+
+}
+
+
+
+console.log('end')
