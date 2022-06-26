8a0eadcc05745d332babfcf0ce540a1ab3c13547
Pakman450 <steven.pak10@gmail.com>
2022-06-25 23:54:45 -0400

    feat: change bg color of download dock

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {
+      colors: {
+        'bg-gray-800-monochromatic': '#314158',
+      },
+     },
+   },
+   plugins: [],
 }
