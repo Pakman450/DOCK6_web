13b0ea690d5a897b86f977e5a831202cb9b033e5
Steven Pak <steven.pak10@gmail.com>
2022-07-07 10:44:16 -0400

    build: change config that deals with typewriting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
    content: ["./src/**/*.{html,js}"],
    theme: {
      extend: {
+      animation: {
+        'typeWriter': 'typingAnim 3.5s steps(40, end), blinkingCursor .5s step-end infinite'
+      },
+
+      keyframes: {
+        typingAnim: {
+          from: { width: '0'},
+          to: { width: '100%' }
+        },
+        blinkingCursor: {
+          from: {borderColor: 'transparent'},
+          // to: {borderColor: 'transparent'},
+          '50%': {borderColor: 'orange'},
+          
+        }
+      },
       colors: {
         'bg-gray-800-monochromatic': '#314158',
         'bg-#5A6679': '#5A6679',
