af33ece17be930f301627fb9b38481a0b8510d92
Pakman450 <steven.pak10@gmail.com>
2022-07-13 12:56:03 -0400

    feat: add end anim time with typewriter

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'typeWriter': 'typingAnim 3s steps(40, end), blinkingCursor .5s step-end 6',
         'typeWriter2': 'hiddenTyping 7s, typingAnim 0s steps(40, end) 7s, blinkingCursor .5s step-end infinite 3.2s, staticCursor 4s 3.2s',
         'gifDelay': 'typingAnim 3.5',
-        'gifHide': 'opacOn step-end 3.3s',
+        'gifHide': 'widthChange 1s infinite 7s, opacOn step-end 3.4s' ,
         'hide1': 'visHide 0s infinite 3.4s',
         'hide2': 'visHide 3.2s',
 
@@ -30,7 +30,14 @@ module.exports = {
         opacOn:{
           from: {opacity: '0'},
           to: {opacity: '100%'}
-
+        },
+        widthChange:{
+          from: {height: '0'},
+          to: {height: '0'}
+        },
+        colorhide:{
+          from: {backgroundColor: 'transparent'},
+          to: {backgroundColor: 'rgb(39 39 42)'}
         },
         blinkingCursor: {
           from: {borderColor: 'transparent'},
