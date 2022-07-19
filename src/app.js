8876882368856ea59870ebc2214fbcd10eadc097
Pakman450 <steven.pak10@gmail.com>
2022-07-19 10:39:02 -0400

    fix: clean up the app.js file

--- src/app.js
+++ src/app.js
@@ -6,24 +6,20 @@ const router = express.Router();
 
 app.use(express.static(__dirname + '/'));
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-
-// I realized you don't download DOCK, you have to 
-// get a license!
-// //get downloadDock.page
-// router.get('/downloadDock',function(req,res){
-//   // res.sendFile(path.join(__dirname+'/index.html'));
-//   res.sendFile(path.join(__dirname+'/downloadDock/downloadDock.html'));
+// router.get('/',function(req,res){
+//   res.download('/Users/stevenpak/Projects/DOCK6_web/src/img/rizzo_logo.png')
+//   res.sendFile(path.join(__dirname+'/index.html'));
 //   //__dirname : It will resolve to your project folder.
 // });
 
+// //add the router
+//app.use('/', router);
 
-//add the router
-app.use('/', router);
+//This is how to download. 
+// app.use('/', (req,res)=>{
+//   res.download('/Users/stevenpak/Projects/DOCK6_web/src/img/rizzo_logo.png')
+// });
+
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
